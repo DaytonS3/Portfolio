314573e2529b4a3f1318bcd5889835eebacbb5fe
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-08-16 16:09:42 -0400

    edit

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import './Home.css';
-import Nav from '../Nav/Nav';
+import React from "react";
+import "./Home.css";
+import Nav from "../Nav/Nav";
 
 function Home() {
   return (
-    <>
+    <div className='container'>
       <Nav />
-      <div class="homeContainer">
-        <div className="introContainer">
-          <p className="introText">Hello, I am</p>
-          <h1 className="overlayText">Dayton Steinmeyer</h1>
-        </div>
-        <p className="HomeSummary">
+      <div class='homeContainer'>
+        <p className='introText'>Hello, I am</p>
+        <h1 className='overlayText'>Dayton Steinmeyer</h1>
+
+        <p className='HomeSummary'>
           I'm a software engineer located in the Atlanta, Georgia area
           specializing in building high-quality websites and applications.
         </p>
-        <div className="infoContainer">
-          <div className="skills">
-            <h2 className="skillsText">Skills</h2>
-            <div className="skillsWrapper">
-              <ul className="sul">
+        <div className='infoContainer'>
+          <div className='skills'>
+            <h2 className='skillsText'>Skills</h2>
+            <div className='skillsWrapper'>
+              <ul className='sul'>
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>JavaScript</li>
               </ul>
-              <ul className="sul">
+              <ul className='sul'>
                 <li>React</li>
                 <li>Redux</li>
                 <li>React-Native</li>
               </ul>
-              <ul className="sul">
+              <ul className='sul'>
                 <li>Node.js</li>
                 <li>Express</li>
                 <li>SQL</li>
@@ -37,21 +36,21 @@ function Home() {
               </ul>
             </div>
           </div>
-          <div className="contactContainer">
-            <h2 className="contactHome">Contact Me</h2>
-            <p className="email">Daytonrsteinmeyer@gmail.com</p>
-            <div className="Icons">
-              <a href="https://github.com/DaytonS3">
-                <i class="fab fa-github fa-3x" />
+          <div className='contactContainer'>
+            <h2 className='contactHome'>Contact Me</h2>
+            <p className='email'>Daytonrsteinmeyer@gmail.com</p>
+            <div className='Icons'>
+              <a href='https://github.com/DaytonS3'>
+                <i class='fab fa-github fa-3x' />
               </a>
-              <a href="https://www.linkedin.com/in/dayton-steinmeyer-a83520174/">
-                <i class="fab fa-linkedin fa-3x" />
+              <a href='https://www.linkedin.com/in/dayton-steinmeyer-a83520174/'>
+                <i class='fab fa-linkedin fa-3x' />
               </a>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
